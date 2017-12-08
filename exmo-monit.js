fa8eb956028e863d860377ae6cd5f8892117a4a6
tomasevich <vyacheslav.tomasevich@gmail.com>
2017-12-08 05:25:12 -0500

    Исправил баг
    Калькуляция теперь проходит корректно, баланс верный

--- exmo-monit.js
+++ exmo-monit.js
@@ -25,7 +25,7 @@
 	})
     }
 
-    var job = schedule.scheduleJob (config.mail.time, () => {
+    var exicute = () => {
 	exmo.query ('user_info', {}, (user_info) => {
 	    if (config.debug) {
 		//console.log ('result:\n', user_info)
@@ -49,17 +49,35 @@
 			amounts[type] = []
 			volumes[type] = []
 			for (curr in user_info[type]) {
-			    if (curr != 'USD' && curr != 'RUB' && user_info[type][curr] > 0) {
+			    let quantity = user_info[type][curr]
+			    let amount = 0
+			    if (quantity > 0) {
+				if (curr == config.currency) {
+				    amount = quantity
+				} else if (curr == 'RUB') {
+				    amount = quantity * ticker['USD_RUB'].buy_price
+				} else if (curr == 'KICK') {
+				    amount = (quantity * ticker['KICK_BTC'].buy_price) * ticker['BTC_USD'].buy_price
+				} else {
+				    if (ticker[curr + '_' + config.currency]) {
+					amount = quantity * ticker[curr + '_' + config.currency].buy_price
+				    }
+				}
+				amount = math.round (amount, 5)
+				amounts[type].push (amount)
 				currencys[type].push (curr)
-				amounts[type].push (math.round ((user_info[type][curr] * ticker[curr + '_' + config.currency].buy_price), 2))
-				volumes[type].push (user_info[type][curr])
-				balance += user_info[type][curr] * ticker[curr + '_' + config.currency].buy_price
+				volumes[type].push (quantity)
+				balance += amount
 			    }
 			}
 		    }
 		}
+		balance = math.round (balance, 2)
 		if (config.debug) {
-		    console.log (currencys, amounts, volumes, balance)
+		    console.log ('currencys\n', currencys)
+		    console.log ('amounts\n', amounts)
+		    console.log ('volumes\n', volumes)
+		    console.log ('balance\n', balance)
 		}
 		template ({
 		    title: {uid: uid, date: date, time: time},
@@ -71,4 +89,10 @@
 	    })
 	})
 	console.log ('exicute:', moment ().format ('hh:mm:ss'))
-    })
+    }
+
+    if (!config.debug) {
+	var job = schedule.scheduleJob (config.mail.time, () => {exicute ()})
+    } else {
+	exicute ()
+    }
